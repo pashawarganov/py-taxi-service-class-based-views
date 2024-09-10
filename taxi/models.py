0e2e58995603f4db67e95f2e530c7bb32a3fe3ec
Pasha <pavik.varganov@gmail.com>
2024-09-10 20:33:33 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.license_number
+
+    def get_absolute_url(self):
+        return reverse("taxi:driver-detail", args=[str(self.id)])
 
 
 class Car(models.Model):
@@ -17,3 +27,9 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def __str__(self):
+        return self.model
+
+    def get_absolute_url(self):
+        return reverse("taxi:car-detail", args=[str(self.id)])
--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    ManufacturerListView,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "manufacturer-list/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+    path("car-list/", CarListView.as_view(), name="car-list"),
+    path("car-detail/<int:pk>", CarDetailView.as_view(), name="car-detail"),
+    path("driver-list/", DriverListView.as_view(), name="driver-list"),
+    path(
+        "driver-detail/<int:pk>",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
 ]
 
 app_name = "taxi"
